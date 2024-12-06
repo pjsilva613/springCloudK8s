bf44c11ed4552909b39a39f9c15ca2390ff7aaec
pjsilva613 <pjsilva613@gmail.com>
2024-12-06 14:16:19 -0500

    se adiciona capas service controller y db al microservicio cursos

---
+++ curso-kubernetes/msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/repositories/CursoRepository.java
@@ -0,0 +1,7 @@
+package org.aguzman.springcloud.msvc.cursos.repositories;
+
+import org.aguzman.springcloud.msvc.cursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+}
