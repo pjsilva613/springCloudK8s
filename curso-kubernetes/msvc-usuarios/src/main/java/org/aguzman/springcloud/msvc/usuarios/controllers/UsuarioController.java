6f256f1484e6f0f6ce75615cde80dce7c2749d64
pjsilva613 <pjsilva613@gmail.com>
2024-12-08 12:48:18 -0500

    se adiciona construccion de la imagen de los microservicios al docker-compose

--- curso-kubernetes/msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ curso-kubernetes/msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -19,7 +19,7 @@ public class UsuarioController {
 
     @GetMapping
     public Map<String, List<Usuario>> listar() {
-        return Collections.singletonMap("users", service.listar());
+        return Collections.singletonMap("usuarios", service.listar());
     }
 
     @GetMapping("/{id}")
